c4429c016157740828cf938716fa93a7703ec9a3
Andrew Strader <astrader@resourceammirati.com>
2017-03-13 11:48:08 -0400

    enable category component

--- src/app/category/category.component.ts
+++ src/app/category/category.component.ts
@@ -1,25 +1,44 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, OnDestroy} from '@angular/core';
 import {PrismModel} from "../prism-model";
 import {PrismStoreService} from "../prism-store.service";
+import {ActivatedRoute} from "@angular/router";
+import {Subscription} from "rxjs";
 
 @Component({
 	selector: 'prism-category',
 	templateUrl: './category.component.html',
 	styleUrls: ['./category.component.scss']
 })
-export class CategoryComponent implements OnInit {
+export class CategoryComponent implements OnInit, OnDestroy {
 
 	private model: PrismModel;
 
-	constructor(private prismStore: PrismStoreService) {
+	private modelSub: Subscription;
+
+	private paramsSub: Subscription;
+
+	private activeCategory: String;
+
+	constructor(private prismStore: PrismStoreService,
+				private route: ActivatedRoute) {
 	}
 
 	public ngOnInit() {
 
-		this.prismStore.model.subscribe((value) => {
+		this.paramsSub = this.route.params.subscribe(params => {
+			let categoryName = params['categoryName'];
+			this.activeCategory = categoryName;
+		});
+
+		this.modelSub = this.prismStore.model.subscribe((value) => {
 			this.model = value;
 		});
 
+	}
+
+	public ngOnDestroy() {
+		this.paramsSub.unsubscribe();
+		this.modelSub.unsubscribe();
 	}
 
 }
