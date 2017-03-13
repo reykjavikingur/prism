58bd57beae0f0c07949bfbcec3d825cc9e9da1e4
Andrew Strader <astrader@resourceammirati.com>
2017-03-13 11:26:38 -0400

    make category list look up its data independently

--- src/app/category-list/category-list.component.ts
+++ src/app/category-list/category-list.component.ts
@@ -1,18 +1,31 @@
-import {Component, OnInit, Input} from '@angular/core';
+import {Component, OnInit, Input, OnDestroy} from '@angular/core';
+import {Subscription} from "rxjs";
+import {PrismStoreService} from "../prism-store.service";
 
 @Component({
 	selector: 'prism-category-list',
 	templateUrl: './category-list.component.html',
 	styleUrls: ['./category-list.component.scss']
 })
-export class CategoryListComponent implements OnInit {
+export class CategoryListComponent implements OnInit, OnDestroy {
 
-	@Input() public categories: Array<String>;
+	private categories: Array<String>;
 
-	constructor() {
+	private sub: Subscription;
+
+	constructor(private prismStore: PrismStoreService) {
 	}
 
 	ngOnInit() {
+
+		this.sub = this.prismStore.model.subscribe(value => {
+			this.categories = value.categories;
+		});
+
+	}
+
+	ngOnDestroy() {
+		this.sub.unsubscribe();
 	}
 
 }
