0031655a4db0dd723fba52a50776f86896c06e7a
Andrew Strader <astrader@resourceammirati.com>
2017-04-01 09:05:14 -0400

    make search form route to search results

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,23 +1,38 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, OnDestroy} from '@angular/core';
 import {PrismStoreService} from "../prism-store.service";
+import {Router} from "@angular/router";
+import {Subscription} from "rxjs";
 
 @Component({
 	selector: 'prism-search',
 	templateUrl: './search.component.html',
 	styleUrls: ['./search.component.scss']
 })
-export class SearchComponent implements OnInit {
+export class SearchComponent implements OnInit, OnDestroy {
 
 	private searchQuery: string;
 
-	constructor(private prismStore: PrismStoreService) {
+	private sub: Subscription;
+
+	constructor(private prismStore: PrismStoreService,
+				private router: Router) {
 	}
 
 	ngOnInit() {
+		this.sub = this.prismStore.activeSearch.subscribe(value => {
+			if (!value) {
+				this.searchQuery = null;
+			}
+		});
+
+	}
+
+	ngOnDestroy() {
+		this.sub.unsubscribe();
 	}
 
 	private doSearch() {
-		this.prismStore.search(this.searchQuery);
+		this.router.navigate(['/guide/search', this.searchQuery]);
 	}
 
 }
